5bc14b69ebc551c2049ad0e902c3b001296b6de6
Kate Hannah De Leon <24-09958@g.batstate-u.edu.ph>
2025-11-28 20:34:40 +0800

    Update PlaceNotFoundException.java

--- Project Lithos/PlaceNotFoundException.java
+++ Project Lithos/PlaceNotFoundException.java
@@ -1,5 +1,8 @@
 public class PlaceNotFoundException extends Exception {
+
+    // Custom exception used when a player tries to explore a place
+    // that is locked, unknown, or not accessible yet.
     public PlaceNotFoundException(String message) {
-        super(message);
+        super(message); // Pass the error message to the Exception superclass
     }
 }
